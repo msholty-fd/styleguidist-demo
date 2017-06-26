d27d016880789cc94fb1a0f544ce096fd5d2c39c
Michael Sholty <michael.sholty@sharpspring.com>
2017-06-26 10:54:11 -0400

    feat(SettingsButton): pass properties down to icon button

--- dist/components/SettingsButton/SettingsButton.js
+++ dist/components/SettingsButton/SettingsButton.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
@@ -41,6 +43,8 @@ var _Colors = require('../../styles/Colors');
 var _Colors2 = _interopRequireDefault(_Colors);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -81,13 +85,17 @@ var SettingsButton = function (_React$Component) {
   }, {
     key: 'render',
     value: function render() {
+      var _props = this.props,
+          children = _props.children,
+          others = _objectWithoutProperties(_props, ['children']);
+
       return _react2.default.createElement(
         _IconButton2.default,
-        {
+        _extends({}, others, {
           iconStyle: styles.icon,
           style: styles.button,
           disableTouchRipple: true
-        },
+        }),
         _react2.default.createElement(_settings2.default, null),
         _react2.default.createElement(_arrowDropDown2.default, null)
       );
